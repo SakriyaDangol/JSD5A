dfb9eb49b4cd0c894c62759e7f1f328a8ba9e637
SakriyaDangol <sakriyadangol003@gmail.com>
2021-06-08 16:44:36 +0545

    commit

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var fail = 'looser';
 
 function getGrade(score) {
     let pass = 'winner';
-    if (score > 0 && score <= 5){
+    if (score > 0 && score <= 25){
         return fail;
-    } else if (score > 25 && score <= 30){
+    } else if (score > 25 && score <= 50){
         return pass;
     }
   }
